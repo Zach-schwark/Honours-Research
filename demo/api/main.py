0a7e9b47b7caba033c08a856e9404a1d3d0cca2e
Zach Schwark <zachs1708@gmail.com>
2024-11-24 17:58:52 +0200

    add more complex main.py

--- demo/api/main.py
+++ demo/api/main.py
@@ -1,43 +1,36 @@
-#import logging
-#import sys
-#import pandas as pd
-#sys.path.insert(0,"../../research")
-#from pgmpy.readwrite import XMLBIFReader
-#from Models import BICBayesianNetwork
-#
-#from fastapi import FastAPI, HTTPException
-#from fastapi.middleware.cors import CORSMiddleware
-#from pydantic import BaseModel
-#
-#app = FastAPI()
-#
+import sys
+sys.path.insert(0,"../../research")
+from pgmpy.readwrite import XMLBIFReader
+from Models import BICBayesianNetwork
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
 ## Set up logging
 #logging.basicConfig(level=logging.INFO)
 #logger = logging.getLogger(__name__)
 #
-## Enable CORS
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=["*"],
-#    allow_credentials=False,
-#    allow_methods=["*"],
-#    allow_headers=["*"],
-#)
-#
-#
-#def load_model():
-#    reader = XMLBIFReader("public/BIC_saved_model.xml")
-#    model = reader.get_model()
-#
-#    BIC_BN = BICBayesianNetwork(model = model)
-#    
-#    return BIC_BN
-#
-#class PredictionInput(BaseModel):
-#    # define your input schema here
-#    variables: dict = {'term': '_36_months', 'installment': '_5_898_435_665_', 'loan_amnt': '_961_0_10750_0_', 'int_rate': '_5_284_11_73_'}
-#
-#
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def load_model():
+    reader = XMLBIFReader("public/BIC_saved_model.xml")
+    model = reader.get_model()
+
+    BIC_BN = BICBayesianNetwork(model = model)
+    
+    return BIC_BN
+
+
 ##@app.post("/predict")
 ##async def predict(input_data: PredictionInput):
 ##    BIC_BN = load_model()
@@ -53,21 +46,20 @@
 ##        logger.error(f"Error during prediction: {str(e)}")
 ##        raise HTTPException(status_code=500, detail=str(e))
 #    
-#@app.get("/")
-#async def root():
-#    return {"message": "Backend is working"}   
-#    
-#    
-#@app.get("/test")
-#async def test():
-#    logger.info("Test endpoint hit")
-#    return {"message": "Backend is working!"}
-
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
 @app.get("/")
 async def root():
-    return {"message": "It works!"}
+    return {"message": "Backend is working"}   
+    
+    
+@app.get("/test")
+async def test():
+    return {"message": "Backend is working!"}
+
+
+#from fastapi import FastAPI
+#
+#app = FastAPI()
+#
+#@app.get("/")
+#async def root():
+#    return {"message": "It works!"}
